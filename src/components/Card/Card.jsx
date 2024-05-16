a5bb2b6c7ce28fbd298fd0fbcb6e0235b126b87e
caue <cauemorenghi14@gmail.com>
2024-05-16 09:26:44 -0300

    implementando estilos globais e propriedades do "tema"

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,18 @@
-import styled from "@emotion/styled";
+import styled from "@emotion/styled"
 
-const StyledDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 32px;
-    gap: 32px;
-    background: #EBEAF9;
-    border: 1px solid #5754ed;
+const CardDiv = styled.div`
+  background-color: ${props => props.theme.cores.secundarias.a};
+  border: solid 1px ${props => props.theme.cores.primarias.a};
+  border-radius: ${props => props.theme.espacamentos.l};
+  padding: 32px;
+  gap: 32px;
+  display: flex;
 `
 
 export const Card = ({ children }) => {
   return (
-    <StyledDiv>
-        {children}
-    </StyledDiv>
-  );
-};
+    <CardDiv>
+      {children}
+    </CardDiv>
+  )
+}
--- src/components/ProvedorTema/ProvedorTema.jsx
+++ src/components/ProvedorTema/ProvedorTema.jsx
@@ -1,32 +1,44 @@
-import { ThemeProvider } from "@emotion/react"
+import { ThemeProvider } from "@emotion/react";
+
 
 const tema = {
     cores: {
+        white: '#FFFFFF',
+        warning: '#FF0E0E',
+        focus: '#B009FF',
+
         primarias: {
-            a: '',
-            b: '',
-            c: ''
+            a: '#5754ED',
+            b: '#D93114',
+            c: '#168070'
         },
-        secundaria: {
-            a: '',
-            b: '',
-            c: ''
+        secundarias: {
+            a: '#EBEAF9',
+            b: '#FDF8F8',
+            c: '#EBFCF9'
         },
-        dark: {
-            a: '',
-            b: '',
-            c: ''
+        escuras: {
+            a: '#110EA0',
+            b: '#B61B00',
         },
-        neutro: {
-            a: '',
-            b: '',
-            c: ''
+        neutras: {
+            a: '#373737',
+            b: '#E8E8E8',
+            c: '#F5F5F5',
         }
-    }
-}
+    },
+    espacamentos: {
+        xxs: '4px',
+        xs: '8px',
+        s: '16px',
+        m: '24px',
+        l: '32px',
+        xl: '48px',
+        xxl: '64px'
+    },
+    fontFamily: "'Montserrat', sans-serif"
+};
 
 export const ProvedorTema = ({ children }) => {
-    return <ThemeProvider theme={tema}>
-        {children}
-    </ThemeProvider>
-}
+  return <ThemeProvider theme={tema}>{children}</ThemeProvider>;
+};
